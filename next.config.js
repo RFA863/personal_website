6b222ffd3a23e8a26fd0fcbe01d6be940a8b81e6
RFA863 <rafifajrul77@gmail.com>
2024-06-04 15:07:57 +0700

    Revert "feat: backgroun video"
    This reverts commit 4ad6da9280cc84ee100621573177a92ed9ddcd5b.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {}
-const { withNextVideo } = require('next-video/process');
-// import withNextVideo from "next-video/process";
 
 module.exports = nextConfig
-module.exports = withNextVideo(nextConfig);
--- src/app/Hero.js
+++ src/app/Hero.js
@@ -1,14 +1,11 @@
 "use client";
 
 import AOS from 'aos';
-import BackgroundVideo from 'next-video/background-video';
-// import Image from "next/image";
-import Video from 'next-video';
+import Image from "next/image";
 import { useEffect } from 'react';
-import { Typewriter } from 'react-simple-typewriter';
-import bgVideo from "./../../videos/bgVidio.mp4";
-// import bgHero from "./../../public/img/bgHero.png";
-// import getStarted from "./../../videos/get-started.mp4"
+import { Typewriter } from 'react-simple-typewriter'
+
+import bgHero from "./../../public/img/bgHero.png";
 
 export default function Landing_Page() {
 
@@ -17,45 +14,40 @@ export default function Landing_Page() {
   }, [])
 
   return (
-    <div id="hero" className=" flex justify-center items-center">
-      <BackgroundVideo src={bgVideo}>
+    <div id="hero" className="relative flex justify-center items-center">
 
+      <div
+        data-aos="zoom-in-down"
+        data-aos-offset="200"
+        data-aos-delay="50"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out"
+        data-aos-mirror="true"
+        data-aos-once="false"
+        data-aos-anchor-placement="top"
+        className="sm:px-10 px-5 py-8 backdrop-blur-lg absolute  z-[1]  rounded-md shadow-2xl shadow-black">
 
-        <div
-          data-aos="zoom-in-down"
-          data-aos-offset="200"
-          data-aos-delay="50"
-          data-aos-duration="1000"
-          data-aos-easing="ease-in-out"
-          data-aos-mirror="true"
-          data-aos-once="false"
-          data-aos-anchor-placement="top"
-          className="sm:px-10 px-5 py-8 backdrop-blur-lg   rounded-md shadow-2xl shadow-black">
-
-          <div className="flex justify-center  sm:text-7xl text-2xl font-semibold border-b-2 border-white pb-5 ">
-            RAFI FAJRUL ARIYADI
-          </div>
-
-          <div className="flex justify-center sm:text-3xl text-md sm:px-4 px-2 py-2">
-            I Am a
-            <span className="ml-2 sm:px-4  rounded-lg  font-semibold">
-              <Typewriter
-                words={['Software Engineer.', 'Software Developer.',
-                  'Web Engineer.', 'Web Developer.', 'Frontend Web Developer.',
-                  'Backend Developer.', 'Full Stack Developer.']}
-                cursor
-                loop={0}
-                typeSpeed={70}
-                deleteSpeed={50}
-                delaySpeed={1000} />
-            </span>
-          </div>
+        <div className="flex justify-center  sm:text-7xl text-2xl font-semibold border-b-2 border-white pb-5 ">
+          RAFI FAJRUL ARIYADI
         </div>
-        {/* <Video src={bgVideo} /> */}
-        {/* <Video src={getStarted} /> */}
-        {/* <Image src={bgHero} className="sm:w-screen object-cover h-screen  " />
-     */}
-      </BackgroundVideo>
+
+        <div className="flex justify-center sm:text-3xl text-md sm:px-4 px-2 py-2">
+          I Am a
+          <span className="ml-2 sm:px-4  rounded-lg  font-semibold">
+            <Typewriter
+              words={['Software Engineer.', 'Software Developer.',
+                'Web Engineer.', 'Web Developer.', 'Frontend Web Developer.',
+                'Backend Developer.', 'Full Stack Developer.']}
+              cursor
+              loop={0}
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000} />
+          </span>
+        </div>
+      </div>
+
+      <Image src={bgHero} className="sm:w-screen object-cover h-screen  " />
     </div>
   );
 }
