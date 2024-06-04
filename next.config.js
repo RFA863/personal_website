4ad6da9280cc84ee100621573177a92ed9ddcd5b
RFA863 <rafifajrul77@gmail.com>
2024-06-04 14:54:23 +0700

    feat: backgroun video

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {}
+const { withNextVideo } = require('next-video/process');
+// import withNextVideo from "next-video/process";
 
 module.exports = nextConfig
+module.exports = withNextVideo(nextConfig);
